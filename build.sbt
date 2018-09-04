192de32757613456cbf7e4acf4295c1f394fc50d
ashwin <ashwin.bhaskar@yatra.com>
2018-09-04 12:28:03 +0530

    Added Scala Redis Client

--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ lazy val akkaVersion = "2.5.16"
 lazy val akkaHttpVersion = "10.1.4"
 lazy val akkaSprayVersion = "10.1.4"
 lazy val akkaStreamVersion = "2.5.16"
-
+lazy val scaladiVersion = "0.5.8"
+lazy val redisClientVersion = "3.7"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
@@ -16,6 +17,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http-spray-json" % akkaSprayVersion,
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
   "com.typesafe.akka" %% "akka-stream" % akkaStreamVersion,
-  "org.scaldi" %%  "scaldi" % "0.5.8",
+  "org.scaldi" %%  "scaldi" % scaladiVersion,
+  "net.debasishg" %% "redisclient" % redisClientVersion,
   "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 )
--- src/main/scala/module/ApplicationModule.scala
+++ src/main/scala/module/ApplicationModule.scala
@@ -4,6 +4,7 @@ import java.io.InputStream
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import com.redis.RedisClient
 import com.typesafe.config.{Config, ConfigFactory}
 import model.Configuration
 import scaldi.Module
@@ -14,9 +15,15 @@ import scala.concurrent.ExecutionContext
 class ApplicationModule extends Module{
   bind[Config] to ConfigFactory.load()
   bind[Configuration] identifiedBy "configuration" to {
-    val stream : InputStream = getClass.getResourceAsStream("/"+ConfigFactory.load().getString("configuration.qa.file"))
+    val stream : InputStream = getClass.getResourceAsStream("/"+ConfigFactory.load().getString("configuration.qa.configFile"))
     val configuration = scala.io.Source.fromInputStream( stream ).mkString
     JsonParser(configuration).convertTo[Configuration]
+  }
+  bind[RedisClient] to {
+    val config = inject[Config]
+    val redisHost = config.getString("configuration.qa.redisHost")
+    val redisPort = config.getString("configuration.qa.redisPort")
+    new RedisClient(host = redisHost, port = redisPort.toInt)
   }
   bind[ActorSystem] to ActorSystem("live-rates-actor-system") destroyWith(_.terminate())
   bind[ExecutionContext] to inject[ActorSystem].dispatcher
